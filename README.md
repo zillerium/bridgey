6c81c0b209456d533caae3d2f4881e135017c861
Trevor Oakley <sales@merrows.co.uk>
2020-10-10 04:15:58 +0000

    initial

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # bridgey
+
+Hackathon 
+
+1. Create data records into a big data db based on device readings
+2. Bridge data into blockchains
+3. Scale Solution
+
+Server API
+
+Test - add gps data into mongodb
+
+curl  -X POST -d @addgps.json -H "Content-Type: application/json" http://3.80.157.235:3000/api/addPos
+
+addgps.json
+{
+"longitude":-84.599453,
+"latitude":39.198647,
+"userid":1234,
+"accesscode":921
+}
+
+Sample Data File - 
+https://data.cincinnati-oh.gov/Efficient-Service-Delivery/Vehicle-GPS-Data-Department-of-Public-Services/b56d-ydmm
+
+MongoDB - 
+
+db.bridgeygps.find()
+{ "_id" : ObjectId("5f812fe6d050496b8538704e"), "Longitude" : 1602301926590, "Latitude" : -84.599453, "UserID" : 39.198647, "GPSDate" : ISODate("1970-01-01T00:00:01.234Z"), "__v" : 0 }
+> 
+
---
+++ api/server.js
@@ -0,0 +1,105 @@
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const app = express();
+//const { Pool, Client } = require('pg')
+const pg = require('pg');
+
+require('dotenv').config();
+
+request = require('request');
+const bodyParser = require('body-parser');
+var wget = require('node-wget');
+var url = require('url');
+var path = require('path');
+
+//const privateKey = fs.readFileSync('privkey.pem', 'utf8');
+//const certificate = fs.readFileSync('fullchain.pem', 'utf8');
+//const ca = fs.readFileSync('cert.pem', 'utf8');
+
+const { Gateway, Wallets } = require('fabric-network');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/bridgeydb');
+
+//const credentials= {
+ // key: privateKey,
+//	cert: certificate
+//}
+
+app.get("/api/ping", function(req, res) {
+    res.json({ message: "pong" });
+});
+
+app.use('/', express.static(path.join(__dirname, '/html')));
+
+//app.use((req, res) => {
+//	res.send('Hello there !');
+//});
+/*app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, x-access-token');
+    next();
+});*/
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var bridgeySchema = new mongoose.Schema({
+  Longitude : { type: Number, default: null },
+  Latitude : { type: Number, default: null},
+  UserID : {type: Number, default: null},
+  GPSDate : {type: Date, default: null},
+});
+   
+var bridgeyDB = mongoose.model("bridgeygps", bridgeySchema);
+
+async function addDB(longitude, latitude, userid, gpsdate) {
+  var bridgeyCreate = new bridgeyDB({
+    Longitude : longitude,
+    Latitude : latitude,
+    UserID : userid,
+    GPSDate : gpsdate
+    });
+    bridgeyCreate.save(function(err, doc){
+        if(err) throw err;
+         console.log("db done");
+      });
+}
+
+app.post("/api/addPos", asyncHandler(async (req, res, next) => {
+
+        var longitude = req.body.longitude;
+        var latitude = req.body.latitude;
+        var userid = req.body.userid;
+        var accesscode = req.body.accesscode;
+        var gpsdate = new Date();
+
+	console.log(longitude);
+	console.log(latitude);
+        addDB(  gpsdate, longitude, latitude, userid) 
+        res.json({"response": "ok"})
+
+}));
+
+// Starting both http & https servers
+//const httpsServer = https.createServer(credentials, app);
+
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(3000, () => {
+    console.log('HTTP Server running on port 3000');
+});
+
+
